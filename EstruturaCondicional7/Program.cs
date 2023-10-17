4df835c00c1c02b4fb329f2e3801e4b73a69f68c
Vitor Paiola <vitinho.souza1234@gmail.com>
2023-10-17 01:37:40 -0300

    Adicionar arquivos de projeto.

---
+++ EstruturaCondicional7/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+string[] vet = Console.ReadLine().Split(' ');
+double x = double.Parse(vet[0], CultureInfo.InvariantCulture);
+double y = double.Parse(vet[1], CultureInfo.InvariantCulture);
+
+if (x == 0 && y == 0) {
+    Console.WriteLine("Origem");
+} else if (x > 0.0 && y > 0.0) {
+    Console.WriteLine("Q1");
+} else if (x < 0.0 && y > 0.0) {
+    Console.WriteLine("Q2");
+} else if (x < 0.0 && y < 0.0) {
+    Console.WriteLine("Q3");
+} else {
+    Console.WriteLine("Q4");
+}
